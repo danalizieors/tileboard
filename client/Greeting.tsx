8a29d2a8baf8390506c74e50e333aabb7c54d488
Danalizi-Erőss Örs <danalizieors@gmail.com>
2020-05-25 00:38:06 +0300

    Add React and styled-components

---
+++ client/Greeting.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type Style = {
+    color: string
+}
+
+const Title = styled.h1<Style>(({ color }) => ({
+    color,
+    textAlign: 'center',
+}))
+
+type Props = {
+    name: string
+}
+
+export const Greeting: React.FC<Props> = ({ name }) => (
+    <Title color='palevioletred'>Hello {name}</Title>
+)
---
+++ client/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Greeting } from './Greeting'
+
+const root = document.getElementById('root')
+ReactDOM.render(<Greeting name='tileboard' />, root)
