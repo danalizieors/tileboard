fdf974c927f67f498ba7cbf1a94b0a0cd6658070
Danalizi-Erőss Örs <danalizieors@gmail.com>
2020-06-08 17:28:10 +0300

    Use custom pointer hook for button

--- client/Button.tsx
+++ client/Button.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { connection, subscribe, unsubscribe } from './connection'
+import { usePointers } from './hooks/usePointers'
 
 export const Button: React.FC = () => {
+    const [pointers, onEvent] = usePointers()
+
     const [state, setState] = useState()
     useEffect(() => {
         const handler = (message: MessageEvent) => setState(message.data)
@@ -10,17 +13,22 @@ export const Button: React.FC = () => {
         return () => unsubscribe(handler)
     }, [])
 
-    const noteOn = () => {
-        connection.send(JSON.stringify({ note: [0b10010000, 64, 100] }))
-    }
-
-    const noteOff = () => {
-        connection.send(JSON.stringify({ note: [0b10000000, 64, 100] }))
-    }
+    useEffect(() => {
+        if (pointers[0]?.action.type === 'down')
+            connection.send(JSON.stringify({ note: [0b10010000, 64, 100] }))
+        if (pointers[0]?.action.type === 'up')
+            connection.send(JSON.stringify({ note: [0b10000000, 64, 100] }))
+    }, [pointers])
 
     return (
         <>
-            <div onPointerDown={noteOn} onPointerUp={noteOff}>
+            <div
+                onPointerEnter={onEvent}
+                onPointerDown={onEvent}
+                onPointerMove={onEvent}
+                onPointerUp={onEvent}
+                onPointerLeave={onEvent}
+            >
                 send note
             </div>
             <div>{state}</div>
